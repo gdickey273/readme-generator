4b136f987b6212966ab3bcc869084b57ad0605fc
gdickey273 <gdickey273@gmail.com>
2020-08-25 09:41:54 -0400

    Started the generate markdown file. I have most sections looking good I just need to add license and a badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,53 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+  // const credits = [];
+  let creditsTemplate = "";
+
+// Graham Dickey gdickey273 | Martin Hemphilll mhemplll | Dianna Mack dmacksdfasdf
+
+  for(let credit of data.credits){
+    creditsTemplate += `* ${credit}\n `
+  };
+  const creditArray = data.credits[0].split(" ").filter(el => el.trim().length > 0);
+  const githubAccount = creditArray.pop();
+
+  
   return `# ${data.title}
 
+  ## Description 
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Credits 
+  ${creditsTemplate}
+
+  ## License 
+  ${data.license}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  Contact me with any questions you have regarding my project.   
+  Email: ${data.email}  
+  Github: https://github.com/${githubAccount}  
 `;
 }
 
